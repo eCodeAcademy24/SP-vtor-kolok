f9de388bc69d2698d8671659f6926c0e5e065748
p_sterjoski <predrag.sterjoski@students.finki.ukim.mk>
2024-12-24 16:13:39 +0100

    summarization 2

--- sumarizacija I/14/main.cpp
+++ sumarizacija I/14/main.cpp
@@ -3,5 +3,43 @@
 using namespace std;
 
 int main() {
+    int n;
+    cin >> n;
+
+    int kol = n * 2;
+    int matrixA[n][kol];
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < kol; j++) {
+            cin >> matrixA[i][j];
+        }
+    }
+
+    int matrixB[kol][n];
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < kol / 2; j++) {
+            matrixB[i][j] = matrixA[i][j];
+        }
+    }
+
+    int redica = kol / 2;
+    for (int i = 0; i < n; i++) {
+        int kolona = 0;
+
+        for (int j = kol / 2; j < kol; j++) {
+            matrixB[redica][kolona] = matrixA[i][j];
+            ++kolona;
+        }
+
+        redica++;
+    }
+
+    for (int i = 0; i < kol; i++) {
+        for (int j = 0; j < n; j++) {
+            cout << matrixB[i][j] << " ";
+        }
+
+        cout << endl;
+    }
+
     return 0;
 }
--- sumarizacija I/7/main.cpp
+++ sumarizacija I/7/main.cpp
@@ -1,8 +1,36 @@
 #include <iostream>
+#include <cmath>
 
 using namespace std;
 
 int main() {
+    int n, m;
+    cin >> n >> m;
 
+    int matrix[100][100];
+    int array[n];
+
+    for (int i = 0; i < n; i++) {
+        int sum = 0;
+        for (int j = 0; j < m; j++) {
+            cin >> matrix[i][j];
+            sum += matrix[i][j];
+        }
+
+        float average = (float) (sum) / m;
+        int furthest = matrix[i][0];
+
+        for (int j = 1; j < m; j++) {
+            if (fabs(matrix[i][j] - average) > fabs(furthest - average)) {
+                furthest = matrix[i][j];
+            }
+        }
+
+        array[i] = furthest;
+    }
+
+    for (int i = 0; i < n; i++) {
+        cout << array[i] << " ";
+    }
     return 0;
 }
