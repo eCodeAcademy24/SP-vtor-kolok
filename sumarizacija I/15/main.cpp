985128d4bca8d3f6973bbe2034a8b3de2a184dcf
p_sterjoski <predrag.sterjoski@students.finki.ukim.mk>
2024-12-25 12:45:59 +0100

    summarization 3

--- sumarizacija I/15/main.cpp
+++ sumarizacija I/15/main.cpp
@@ -2,7 +2,46 @@
 
 using namespace std;
 
-int main() {
+int poramnet(int number) {
+    if (number == 0) { // стоп условот
+        return 0;
+    }
 
+    int last_digit = number % 10;
+    int remaining = poramnet(number / 10);
+
+    if (last_digit == 9) {
+        return remaining * 10 + 7;
+    } else {
+        return remaining * 10 + last_digit;
+    }
+}
+
+void sort(int array[], int n) {
+    for (int i = 0; i < n; i++) {
+        for (int j = i + 1; j < n; j++) {
+            if (array[i] > array[j]) {
+                swap(array[i], array[j]);
+            }
+        }
+    }
+}
+
+int main() {
+    int broj;
+    int poramneti_broevi[100];
+    int i = 0;
+    while (cin >> broj) {
+        poramneti_broevi[i++] = poramnet(broj);
+    }
+
+    sort(poramneti_broevi, i);
+
+//    if (i > 5) {
+//        i = 5;
+//    }
+    for (int j = 0; j < (i <= 5 ? i : 5); j++) {
+        cout << poramneti_broevi[j] << " ";
+    }
     return 0;
 }
--- sumarizacija I/9/main.cpp
+++ sumarizacija I/9/main.cpp
@@ -1,8 +1,25 @@
 #include <iostream>
+#include <iomanip>
 
 using namespace std;
 
-int main() {
+double recursion(int *array, int n) {
+    if (n == 1) { // стоп условот
+        return array[0]; // *(array)
+    } else {
+        return array[0] + (1 / recursion(array + 1, n - 1));
+    }
+}
 
+int main() {
+    int n;
+    cin >> n;
+
+    int array[100];
+    for (int i = 0; i < n; i++) {
+        cin >> array[i];
+    }
+
+    cout << fixed << setprecision(6) << recursion(array, n);
     return 0;
 }
