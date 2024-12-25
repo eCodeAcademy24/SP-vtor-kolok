454f129f7dd1f0220dbd53c750a3def0df4ed20a
p_sterjoski <predrag.sterjoski@students.finki.ukim.mk>
2024-12-25 19:47:08 +0100

    summarization 5

--- sumarizacija II/10/main.cpp
+++ sumarizacija II/10/main.cpp
@@ -1,8 +1,73 @@
 #include <iostream>
+#include <cstdlib>
 
 using namespace std;
 
+void transform(int *array, int n) {
+    int shifter = array[0];
+    if (shifter < 0) { // shift left
+        for (int i = 0; i < abs(shifter); i++) {
+            for (int j = 0; j < n; j++) {
+                array[j] = array[j + 1];
+            }
+
+            array[n - 1] = 0;
+        }
+    } else { // shift right
+        for (int i = 0; i < abs(shifter); i++) {
+            for (int j = n - 1; j > 0; j--) {
+                array[j] = array[j - 1];
+            }
+
+            array[0] = 0;
+        }
+    }
+}
+
+void printer(int *array, int n) {
+    for (int i = 0; i < n; i++) {
+        cout << array[i] << " ";
+    }
+
+    cout << endl;
+}
+
+bool zeroCounter(int *array, int n) {
+    int counter = 0;
+    for (int i = 0; i < n; i++) {
+        if (array[i] == 0) {
+            ++counter;
+        }
+    }
+
+//    if (counter == n) return true;
+//
+//    return false;
+    return counter == n;
+}
+
 int main() {
-    
+    int m;
+    cin >> m;
+
+    int n;
+    int array[100];
+    int zeros = 0;
+
+    for (int i = 0; i < m; i++) {
+        cin >> n;
+        for (int j = 0; j < n; j++) {
+            cin >> array[j];
+        }
+
+        transform(array, n);
+        printer(array, n);
+
+        if (zeroCounter(array, n)) {
+            zeros++;
+        }
+    }
+
+    cout << zeros;
     return 0;
 }
--- sumarizacija II/9/main.cpp
+++ sumarizacija II/9/main.cpp
@@ -3,7 +3,47 @@
 
 using namespace std;
 
-int main() {
+int svrznik(char *line) {
+    int brojac_svrznici = 0; // бројач на сврзници во една линија
+    int brojac_bukvi = 0; // бројач на букви во еден збор
 
+    for (int i = 0; i < strlen(line); i++) {
+        if (isspace(line[i]) || !isalnum(line[i])) {
+            if (brojac_bukvi >= 1 && brojac_bukvi <= 3) {
+                brojac_svrznici++;
+            }
+            brojac_bukvi = 0;
+        } else {
+            brojac_bukvi++;
+        }
+    }
+
+    return brojac_svrznici;
+}
+
+int main() {
+    char line[101], max_line[101];
+    int max = 0;
+    int flag = true;
+
+    while (cin.getline(line, 101)) {
+        if (strcmp(line, "#") == 0) break;
+
+        int svrznici = svrznik(line);
+
+        if (flag) {
+            max = svrznici;
+            strcpy(max_line, line);
+            flag = false;
+            continue;
+        }
+
+        if (svrznici > max) {
+            max = svrznici;
+            strcpy(max_line, line);
+        }
+    }
+
+    cout << max << ": " << max_line;
     return 0;
 }
