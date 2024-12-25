645dcd3940bbabc92c63ae5219b7eb5db757c586
p_sterjoski <predrag.sterjoski@students.finki.ukim.mk>
2024-12-25 18:35:40 +0100

    summarization 4

--- sumarizacija II/2/main.cpp
+++ sumarizacija II/2/main.cpp
@@ -4,6 +4,42 @@
 using namespace std;
 
 int main() {
+    int n;
+    cin >> n;
 
+    int m = (int) (ceil(sqrt(n)));
+
+    int element = 1;
+    int matrix[m][m];
+
+    for (int j = 0; j < m; j++) {
+        if (j % 2 == 0) { // парен индекс (горе-надолу)
+            for (int i = 0; i < m; i++) {
+                if (element <= n) {
+                    matrix[i][j] = element;
+                    element++;
+                } else {
+                    matrix[i][j] = 0;
+                }
+            }
+        } else { // непарен индекс (доле-нагоре)
+            for (int i = m - 1; i >= 0; i--) {
+                if (element <= n) {
+                    matrix[i][j] = element;
+                    element++;
+                } else {
+                    matrix[i][j] = 0;
+                }
+            }
+        }
+    }
+
+    for (int i = 0; i < m; i++) {
+        for (int j = 0; j < m; j++) {
+            cout << matrix[i][j] << " ";
+        }
+
+        cout << endl;
+    }
     return 0;
 }
--- sumarizacija II/6/main.cpp
+++ sumarizacija II/6/main.cpp
@@ -3,81 +3,82 @@
 
 using namespace std;
 
-void printMatrix(float matrix[100][100], int n, int m) {
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j < m; j++) {
-            cout << fixed << setprecision(2) << matrix[i][j] << " ";
-        }
-        cout << "\n";
-    }
-}
-
 int main() {
     int n, m;
-    float matrix[100][100], helpMatrix[100][100];
+    float matrix[100][100], help_matrix[100][100];
 
     cin >> n >> m;
-
-    for(int i = 0; i < n; i++){
-        for(int j = 0; j < m; j++){
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
             cin >> matrix[i][j];
         }
     }
 
     int indexI1, indexI2, indexJ1, indexJ2;
-
     cin >> indexI1 >> indexJ1;
     cin >> indexI2 >> indexJ2;
 
-    float sum;
-    int counter;
+    for (int i = indexI1; i <= indexI2; i++) {
+        for (int j = indexJ1; j <= indexJ2; j++) {
+            float sum = matrix[i][j];
+            int counter = 1;
 
-    for(int i = indexI1; i <= indexI2; i++){
-        for(int j = indexJ1; j <= indexJ2; j++){
-            sum = matrix[i][j];
-            counter = 1;
-            if (i > 0){ // goren
+            if (i > 0) { // горен
                 sum += matrix[i - 1][j];
                 counter++;
             }
-            if(i < n - 1) { // dole
+
+            if (i < n - 1) { // долен
                 sum += matrix[i + 1][j];
                 counter++;
             }
-            if(j > 0) { // levo
+
+            if (j > 0) { // лево
                 sum += matrix[i][j - 1];
                 counter++;
             }
-            if(j < m - 1) { // desno
+
+            if (j < m - 1) { // десно
                 sum += matrix[i][j + 1];
                 counter++;
             }
-            if (i > 0 && j > 0) { // gore levo
+
+            if (i > 0 && j > 0) { // горе лево
                 sum += matrix[i - 1][j - 1];
                 counter++;
             }
-            if(i < n - 1 && j < m - 1) { // dole desno
+
+            if (i < n - 1 && j < m - 1) { // доле десно
                 sum += matrix[i + 1][j + 1];
                 counter++;
             }
-            if(i > 0 && j < m - 1){ // gore desno
+
+            if (i > 0 && j < m - 1) { // горе десно
                 sum += matrix[i - 1][j + 1];
                 counter++;
             }
-            if(i < n - 1 && j > 0){ // dole levo
+
+            if (i < n - 1 && j > 0) { // доле лево
                 sum += matrix[i + 1][j - 1];
                 counter++;
             }
-            helpMatrix[i][j] = sum / counter;
+
+            help_matrix[i][j] = sum / counter;
         }
     }
 
-    for(int i = indexI1; i <= indexI2; i++){
-        for(int j = indexJ1; j <= indexJ2; j++){
-            matrix[i][j] = helpMatrix[i][j];
+    for (int i = indexI1; i <= indexI2; i++) {
+        for (int j = indexJ1; j <= indexJ2; j++) {
+            matrix[i][j] = help_matrix[i][j];
         }
     }
 
-    printMatrix(matrix, n, m);
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
+            cout << fixed << setprecision(2) << matrix[i][j] << " ";
+        }
+
+        cout << endl;
+    }
     return 0;
 }
